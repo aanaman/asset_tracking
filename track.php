2e38a6b0293870cb0033880e5969946a12a51dd6
Abdullah Anaman <42936448+berrydeblaq@users.noreply.github.com>
2020-09-08 19:13:53 +0000

    Update track.php

--- track.php
+++ track.php
@@ -14,7 +14,7 @@ include ('includes/configpdo.php');
         <link rel="stylesheet" href="css/bootstrap.min.css" media="screen" >
         <link rel="stylesheet" href="css/font-awesome.min.css" media="screen" >
         <link rel="stylesheet" href="css/animate-css/animate.min.css" media="screen" >
-        <link rel="stylesheet" href="css/prism/prism.css" media="screen" > <!-- USED FOR DEMO HELP - YOU CAN REMOVE IT -->
+        <link rel="stylesheet" href="css/prism/prism.css" media="screen" >
         <link rel="stylesheet" href="css/main.css" media="screen" >
         <link rel="stylesheet" href="css/navbar.css" media="screen">
         <script src="js/modernizr/modernizr.min.js"></script>
